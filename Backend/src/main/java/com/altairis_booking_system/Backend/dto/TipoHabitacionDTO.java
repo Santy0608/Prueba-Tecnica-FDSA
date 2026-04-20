edcf7307c60ec85b75e4926335fd75122993f41d
Santy0608 <barquerotorress@gmail.com>
2026-04-20 16:51:59 -0600

    feat (Backend): Desarrollando la clase de TipoHabitacionDTO

---
+++ Backend/src/main/java/com/altairis_booking_system/Backend/dto/TipoHabitacionDTO.java
@@ -0,0 +1,93 @@
+package com.altairis_booking_system.Backend.dto;
+
+import jakarta.persistence.Column;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class TipoHabitacionDTO {
+
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private Integer capacidad;
+    private BigDecimal precioBase;
+    private boolean activo;
+    private LocalDateTime createdAt;
+
+    //Campos para la asociación con hotel.
+    private Long hotelId;
+    private String nombreHotel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Integer getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(Integer capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public BigDecimal getPrecioBase() {
+        return precioBase;
+    }
+
+    public void setPrecioBase(BigDecimal precioBase) {
+        this.precioBase = precioBase;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(Long hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public String getNombreHotel() {
+        return nombreHotel;
+    }
+
+    public void setNombreHotel(String nombreHotel) {
+        this.nombreHotel = nombreHotel;
+    }
+}
