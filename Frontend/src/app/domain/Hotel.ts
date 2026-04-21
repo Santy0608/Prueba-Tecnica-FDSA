b88d3c012a4ee8a5ee6ad22f5ee25d949d01a5df
Santy0608 <barquerotorress@gmail.com>
2026-04-20 21:56:08 -0600

    feat (Frontend): Configuración del entorno y desarrollo de la clase de Hotel con Angular

---
+++ Frontend/src/app/domain/Hotel.ts
@@ -0,0 +1,13 @@
+export class Hotel{
+    idHotel!: number;
+    nombre?: string;
+    descripcion?: string;
+    ciudad?: string;
+    pais?: string;
+    telefono?: string;
+    email?: string;
+    estrellas?: number;
+    activo?: boolean;
+
+    
+}
