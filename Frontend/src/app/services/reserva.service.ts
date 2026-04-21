6dcf256000a4b3d486c6584933fdd8bb39d17391
Santy0608 <barquerotorress@gmail.com>
2026-04-21 00:54:16 -0600

    feat (Frontend): Desarrollo del servicio de Reservas

---
+++ Frontend/src/app/services/reserva.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Reserva } from '../domain/Reserva';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservaService {
+
+  private url = 'http://localhost:8080/api/reservas';
+
+  constructor(private http: HttpClient) {}
+
+  listadoReservas(): Observable<Reserva[]> {
+    return this.http.get<Reserva[]>(this.url);
+  }
+
+  buscarReservaPorId(id: number): Observable<Reserva> {
+    return this.http.get<Reserva>(`${this.url}/${id}`);
+  }
+
+  guardarReserva(reserva: Reserva): Observable<Reserva> {
+    return this.http.post<Reserva>(this.url, reserva);
+  }
+
+  editarReserva(reserva: Reserva): Observable<Reserva> {
+    return this.http.put<Reserva>(`${this.url}/${reserva.idReserva}`, reserva);
+  }
+
+  actualizarEstado(id: number, estado: string): Observable<void> {
+    return this.http.patch<void>(`${this.url}/${id}/estado?estado=${estado}`, {});
+  }
+
+  eliminarReserva(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.url}/${id}`);
+  }
+}
